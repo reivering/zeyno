a350a4b000b5d2c681f21880ece7a035447583ca
Abel Varghese <abelshinevarghese@gmail.com>
2025-07-11 04:02:36 +0400

    feat: remade Home page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,16 +3,17 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-black z-50">
-      <div className="mx-auto flex py-4 items-center justify-between" style={{ width: '70vw' }}>
-        <span className="font-extrabold text-white text-3xl tracking-tight" style={{ fontFamily: 'Inter, sans-serif' }}>
-          ZEYNO
-        </span>
-        <div className="flex space-x-10 font-inter">
-          <a href="#about" className="text-white font-normal text-lg hover:underline transition-all">About</a>
-          <a href="#services" className="text-white font-normal text-lg hover:underline transition-all">Services</a>
-          <a href="#contact" className="text-white font-normal text-lg hover:underline transition-all">Contact</a>
-        </div>
+    <nav className="bg-background top-0 left-0 w-full z-50">
+      <div className="mx-auto flex py-4 items-center justify-between w-[80vw]" >
+      <span className="font-bold  text-2xl text-text tracking-tight font-inter" >
+            <span className="block text-xs text-secondary font-normal" style={{ letterSpacing: '0.1em' }}>STUDIO</span>
+            <h1 className="text-base text-[2vw]" style={{lineHeight: "0.6em"}} >zeyno</h1>
+          </span>
+        <nav className="flex space-x-8 font-inter">
+            <a href="#about" className=" text-base font-medium hover:underline">about</a>
+            <a href="#services" className=" text-base font-medium hover:underline">services</a>
+            <a href="#contact" className=" text-base font-medium hover:underline">contact</a>
+          </nav>
       </div>
     </nav>
   );
--- components/pages/About.jsx
+++ components/pages/About.jsx
@@ -3,28 +3,29 @@ import { BackgroundGradientAnimation } from "../ui/background-gradient-animation
 
 const About = () => {
   return (
-      <main id="about" className="flex flex-col items-center justify-center min-h-screen w-full pt-32 bg-black">
-        <section className="w-[75vw]  px-4">
-          <h1 className="text-white  text-3xl md:text-4xl lg:text-6xl font-inter font-extralight leading-snug  mb-16">
-            Architecting digital experiences that captivate, <br className="hidden md:inline" />
-            convert and inspire through seamless functionality, <br className="hidden md:inline" />
-            sophisticated design and strategic vision that <br className="hidden md:inline" />
-            transforms brands into digital powerhouses.
-          </h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-28 mb-20">
-            <p className="text-white text-2xl md:text-4xl font-inter font-light text-left">
-              Zeyno Studios crafts websites that don't just function, they mesmerize and drive meaningful results.
-            </p>
-            <p className="text-white text-2xl md:text-4xl font-inter font-light text-left md:text-left">
-              We architect digital ecosystems where form meets function in perfect harmony.
-            </p>
+      <main id="about" className="flex flex-col items-center bg-background justify-center min-h-screen w-full pt-32">
+        <section className="w-[85vw] h-[75vh] border bg-black flex items-center px-4 rounded-[5vw]">
+          <div className="flex border-r w-[55%] h-[75%] ml-24 flex items-center" >
+            <h1 className="text-white  text-4xl md:text-4xl lg:text-[3vw] font-inter font-extralight leading-snug  mb-16 " style={{lineHeight: "0.9em"}}>
+              Architecting digital experiences that captivate,
+              convert and inspire through <span className="text-base text-4xl md:text-4xl lg:text-[3vw]">seamless functionality, 
+              sophisticated design and strategic vision</span> that 
+              transforms brands into digital powerhouses.
+            </h1>
+
           </div>
+
+          <div className="flex flex-col items-center justify-center  w-[35%] h-[75%]">
+            <div className="flex flex-col  p-12" >
+              <span className="text-base pl-1 text-[30px] font-inter font-normal tracking-widest mb-1">STUDIO</span>
+              <span className="text-white text-8xl font-bold mb-4 font-inter" style={{lineHeight: "0.5em"}}>zeyno</span>
+            </div>
+            <span className="text-lg font-inter font-light text-white mt-2">
+              a <span className="text-[#f47c4c]">creative</span> agency
+            </span>
+          </div>
+          
         </section>
-        <div className="w-full flex justify-center items-end flex-1">
-          <span className="text-white text-[10vw] md:text-[12vw] lg:text-[14vw] font-bold font-archivo lowercase leading-none opacity-90 select-none">
-            about
-          </span>
-        </div>
       </main>
   );
 };
