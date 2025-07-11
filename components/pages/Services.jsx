fef1ff1e9cee5ef472ae2f14efcafc1092675f53
Abel Varghese <abelshinevarghese@gmail.com>
2025-07-11 21:56:16 +0400

    feat: finished service page

--- components/pages/Services.jsx
+++ components/pages/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { MdOutlinePalette, MdOutlineSettings, MdLightbulbOutline, MdArrowOutward } from "react-icons/md";
 
 const Services = () => {
   return (
@@ -10,9 +11,9 @@ const Services = () => {
                 <h1 className='text-base  font-semibold lg:text-8xl sm:text-4xl' style={{lineHeight: "0.9em"}}>services</h1>
                 <p className='font-extrabold lg:text-xl'>DESIGN TRIFECTA</p>
             </div>
-            <div className='flex items-end gap-3 mr-16 w-64 cursor-pointer '>
-                <h1 className='p-4 bg-black rounded-full text-[27px] font-extralight px-6   text-base'>know more</h1>
-                <span className='p-4 bg-black rounded-full text-xl font-normal text-base'>↗️</span>
+            <div className='flex items-end gap-3 mr-16 cursor-pointer '>
+                <h1 className='p-3 bg-black rounded-full text-[24px] text-[var(--color-base)] font-extralight px-8 '>know more</h1>
+                <span className='p-3 bg-black rounded-full text-4xl font-normal text-[var(--color-base)]'><MdArrowOutward /></span>
             </div>
 
         </div>
@@ -21,24 +22,24 @@ const Services = () => {
         <div className='flex justify-center p-4  mt-[3vh] w-full h-[70%] gap-16'>
             {/* Design Card */}
             <div className='w-[500px] h-[55vh] rounded-[7vh] bg-base flex flex-col p-14'>
-              <span className="text-3xl mb-4">🎨</span>
-              <h2 className="text-black text-4xl mt-[48%] font-light mb-2">Design</h2>
+              <span className="text-5xl mb-4"><MdOutlinePalette /></span>
+              <h2 className="text-black text-4xl mt-[44%] font-light mb-2">Design</h2>
               <p className="text-black text-base text-lg font-light">
                 We craft clean, responsive, and fast-loading websites that blend modern UX/UI, accessibility, and branding—prioritizing thoughtful design that guides users and drives conversions without compromising usability.
               </p>
             </div>
             {/* Intelligence Card */}
             <div className='w-[500px] h-[55vh] rounded-[7vh] bg-base flex flex-col p-14'>
-              <span className="text-3xl mb-4">💡</span>
-              <h2 className="text-black text-4xl mt-[48%] font-light mb-2">Intelligence</h2>
+              <span className="text-5xl mb-4"><MdLightbulbOutline /> </span>
+              <h2 className="text-black text-4xl mt-[44%] font-light mb-2">Intelligence</h2>
               <p className="text-black text-base text-lg font-light">
                 We embed AI into websites, CRMs, and operations to automate workflows, enhance decision-making, and uncover insights that drive smarter, more meaningful user experiences.
               </p>
             </div>
             {/* Integration Card */}
             <div className='w-[500px] h-[55vh] rounded-[7vh] bg-base flex flex-col p-14'>
-              <span className="text-3xl mb-4">⚙️</span>
-              <h2 className="text-black text-4xl mt-[48%] font-light mb-2">Integration</h2>
+              <span className="text-5xl mb-4"><MdOutlineSettings /></span>
+              <h2 className="text-black text-4xl mt-[44%] font-light mb-2">Integration</h2>
               <p className="text-black text-base text-lg font-light">
                 We build unified digital ecosystems—integrating websites, CRMs, APIs, and dashboards into seamless, scalable systems that eliminate silos, automate workflows, and align marketing, support, and sales.
               </p>
