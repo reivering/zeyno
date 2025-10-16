3ada2af7c267623f8ba57341b5d6c7c57c0a1091
Abel Varghese <abelshinevarghese@gmail.com>
2025-10-16 06:12:36 +0530

    fix: google site verification added to metadata

--- app/layout.js
+++ app/layout.js
@@ -62,7 +62,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="google202ef2826eb743db.html.html" />
+        <meta name="google-site-verification" content="google202ef2826eb743db.html" />
       </head>
       <body className={`${playfairDisplay.variable} ${work_sans.variable} ${raleway.variable} ${geist.variable} ${inter.variable} ${syne.variable} ${archivo.variable} ${montserrat.variable} text-black bg-white min-h-screen`}>{children}</body>
     </html>
